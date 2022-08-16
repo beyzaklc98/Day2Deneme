778bcc544ebf5568af6d0cdb8e7cab40d256fb2a
Beyza Kılıç <beyza7259@gmail.com>
2022-08-16 20:59:04 +0300

    K

--- src/Day3GitGithub.java
+++ src/Day3GitGithub.java
@@ -5,5 +5,6 @@ public class Day3GitGithub {
         System.out.println("3. Gun 1. Deneme");
         System.out.println("3. Gun 2. Deneme");
         System.out.println("3. Gun 3. Branch Deneme");
+        System.out.println("3. Gun 4. Master Deneme");
     }
 }
